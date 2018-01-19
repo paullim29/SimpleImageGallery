7378c2136f6bce6df65512dde425fad2ca3acc0d
Paul <paullim29@gmail.com>
2018-01-19 20:02:47 +0800

    Create service layer, implement ImageService in controller

---
+++ SimpleImageGallery.Data/IImage.cs
@@ -0,0 +1,12 @@
+﻿using SimpleImageGallery.Data.Models;
+using System.Collections.Generic;
+
+namespace SimpleImageGallery.Data
+{
+    public interface IImage
+    {
+        IEnumerable<GalleryImage> GetAll();
+        IEnumerable<GalleryImage> GetWithTag(string tag);
+        GalleryImage GetById(int id);
+    }
+}
---
+++ SimpleImageGallery/Controllers/GalleryController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleImageGallery.Models;
+using SimpleImageGallery.Data.Models;
+using SimpleImageGallery.Data;
+
+namespace SimpleImageGallery.Controllers
+{
+    public class GalleryController : Controller
+    {
+        private readonly IImage _imageService;
+
+        public GalleryController(IImage imageService)
+        {
+            _imageService = imageService;
+        }
+
+        public IActionResult Index()
+        {
+            var imageList = _imageService.GetAll();
+
+            var model = new GalleryIndexModel()
+            {
+                Images = imageList,
+                SearchQuery = ""
+            };
+
+            return View(model);
+        }
+    }
+}
